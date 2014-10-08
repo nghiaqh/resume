cafa3798d37f1de394acce6461877c52dea483ee
Nathan Quach <nathan.quach@vimn.com>
2014-10-08 17:02:01 +1100

    update font, style, add call for action banner

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing');
+
+  });
+});
